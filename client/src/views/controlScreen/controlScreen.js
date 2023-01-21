b725e1b428456568b7f5c4947178d5158dd42667
Peter Giamalakis <giamalakisp@outlook.com>
2023-01-22 01:19:09 +0200

    Added navbar + partial messagebox functionality

--- client/src/views/controlScreen/controlScreen.js
+++ client/src/views/controlScreen/controlScreen.js
@@ -1,7 +1,32 @@
 const { ipcRenderer } = require('electron')
+const { MDCTextField } = require('@material/textfield');
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 const titleCell = document.getElementById("title")
 
+let active = "messageBox"
+
+const useEffect = () => {
+    document.getElementById(active+"-component").style.display = "block"
+}
+useEffect()
+
+const handleSelection = (screen) => {
+    document.getElementById(active).classList.add("inactive")
+    document.getElementById(active + "-icon").classList.add("inactive")
+    document.getElementById(active).classList.remove("active")
+    document.getElementById(active + "-icon").classList.remove("active")
+    document.getElementById(active + "-component").style.display = "none"
+    active = screen
+    document.getElementById(active).classList.add("active")
+    document.getElementById(active + "-icon").classList.add("active")
+    document.getElementById(active).classList.remove("inactive")
+    document.getElementById(active + "-icon").classList.remove("inactive")
+
+    document.getElementById(active + "-component").style.display = "block"
+
+}
 
 ipcRenderer.send("get-server")
 
